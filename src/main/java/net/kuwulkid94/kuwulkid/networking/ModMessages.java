bde90e0184823940b0890c83e386a2f86dff11ee
koolkid94 <alexander.yuchi@gmail.com>
2023-02-13 23:09:56 -0800

    Fixed Critical Bug

--- src/main/java/net/kuwulkid94/kuwulkid/networking/ModMessages.java
+++ src/main/java/net/kuwulkid94/kuwulkid/networking/ModMessages.java
@@ -4,12 +4,12 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.kuwulkid94.kuwulkid.networking.packet.ItemStackSyncS2CPacket;
 
 import static net.kuwulkid94.kuwulkid.JustaFantasyAddonClientMod.ITEM_SYNC;
-import static net.kuwulkid94.kuwulkid.JustaFantasyAddonClientMod.PacketID;
+//import static net.kuwulkid94.kuwulkid.JustaFantasyAddonClientMod.PacketID;
 
 public class ModMessages {
 
     public static void registerS2CPackets() {
         ClientPlayNetworking.registerGlobalReceiver(ITEM_SYNC, ItemStackSyncS2CPacket::receive);
-        ClientPlayNetworking.registerGlobalReceiver(PacketID, ItemStackSyncS2CPacket::receive);
+        //ClientPlayNetworking.registerGlobalReceiver(PacketID, ItemStackSyncS2CPacket::receive);
     }
 }
